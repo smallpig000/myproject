8d2c0ff16be3aba9d94fc8b60572b39519e0823e
smallpig <smallpig0920769043@gmail.com>
2020-04-18 11:18:58 +0800

    Java and Kotlin variable, data type

--- src/main/java/com/hank/Hello.java
+++ src/main/java/com/hank/Hello.java
@@ -6,5 +6,17 @@ public class Hello {
 //        new Person().hello();
         Person p = new Person();
         p.hello();
+        //基本資料型態
+        int age = 19;
+        char c = 'A';
+        byte b = 120;
+        float weight = 66.5f;
+        float height = 1.7f;
+        boolean adult = true;
+        boolean enroll = false;
+        //參照資料型態
+        Integer age2 = 19;
+        Character c2 = 'A';
+        String name = "Tom";
     }
 }
